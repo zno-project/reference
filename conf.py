e5606a6937523206f2677a09ac688029cdb8a549
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2021-12-12 17:54:45 +0000

    Add sphinx-panels

--- conf.py
+++ conf.py
@@ -36,6 +36,7 @@ release = '0'
 extensions = [
         "sphinx_rtd_theme",
         "myst_parser",
+        "sphinx_panels"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
